4c8246d07d4a0bd0f3a5f816d67f2db2a6951b8b
Thorsten Ruprechter <th.ruprechter@gmail.com>
2016-05-31 09:44:16 +0200

    new activity

---
+++ app/src/main/java/rup/tho/cocook/about/AboutActivity.java
@@ -0,0 +1,15 @@
+package rup.tho.cocook.about;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rup.tho.cocook.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
